6c8603ece1e6f4b142aae2a43dfdac957bac1fcc
zsperske@gmail.com <zsperske@gmail.com>
2017-02-21 20:03:37 -0800

    Added positive test for get submissions

--- tests/test_submissions.py
+++ tests/test_submissions.py
@@ -1,5 +1,15 @@
+import os
+
+import pycrowd
+
 
 class TestSubmissions(object):
 
     def test_get_submission_for_bounty_passes(self):
-        assert 1 == 1
+        uname = os.environ.get('BCUSER')
+        pw = os.environ.get('BCPW')
+
+        test = pycrowd.Bugcrowd(uname, pw)
+        r = test.get_submissions_for_bounty("84b71b04-a363-441f-91e0-8519ad3a4f4f")
+
+        assert r.status_code == 200
