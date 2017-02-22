d4a762bd9dddff01f2eb4bf1fb3bdde9eae0dab3
zsperske@gmail.com <zsperske@gmail.com>
2017-02-22 00:24:26 -0800

    Updated disabled test

--- tests/test_bounties.py
+++ tests/test_bounties.py
@@ -46,7 +46,6 @@ class TestBounties(object):
         test = pycrowd.Client(uname, pw)
         r = test.get_custom_fields_for_bounty("84b71b04-a363-441f-91e0-8519ad3a4f4f")
 
-
         assert r.status_code == 200
 
     # TODO reenable by fetching field ID after creation then deleting
@@ -56,12 +55,13 @@ class TestBounties(object):
 
     #    test = pycrowd.Client(uname, pw)
     #    r = test.create_custom_field_for_bounty("84b71b04-a363-441f-91e0-8519ad3a4f4f", "newField")
+    #    assert r.status_code = 201
 
     #    print(r.text)
 
     #    r = test.delete_custom_field_for_bounty("84b71b04-a363-441f-91e0-8519ad3a4f4f",
     #                                            "021d9127-8088-4477-b124-0e961c2a785c")
-    #    assert r.status_code == 201
+    #    assert r.status_code == 200
 
     def test_update_custom_field_label_for_bounty(self):
         uname = os.environ.get('BCUSER')
