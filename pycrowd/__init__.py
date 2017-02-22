eb03126055b093c9b102231141b1fd56f2dd093f
Leif Dreizler <leifdreizler@gmail.com>
2017-02-21 18:20:26 -0800

    weird pathing fix

--- pycrowd/__init__.py
+++ pycrowd/__init__.py
@@ -1,4 +1,1 @@
-from .bugcrowd import Bugcrowd
-
-__all__ = ['Bugcrowd']
 __version__ = '0.1'
--- tests/test_pycrowd.py
+++ tests/test_pycrowd.py
@@ -1,3 +1,7 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
 import pycrowd
 
 class TestPycrowd(object):
